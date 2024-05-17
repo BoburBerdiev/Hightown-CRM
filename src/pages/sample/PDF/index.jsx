568ba404a8ecfd9dcbf98e529aa8c068b091c829
BoburBerdiev <sirofim012@gmail.com>
2024-05-17 15:34:03 +0500

    full

---
+++ src/pages/sample/PDF/index.jsx
@@ -0,0 +1,15 @@
+import { PDFViewer} from '@react-pdf/renderer';
+import CreatPDF from "../Selling/creat-PDF";
+
+const PDF = () => {
+
+  return (
+      <div>
+          <PDFViewer style={{ width: '100%', height: '700px' }}>
+              <CreatPDF  />
+          </PDFViewer>
+      </div>
+  );
+};
+
+export default PDF;
